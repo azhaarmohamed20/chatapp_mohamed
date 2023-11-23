b8844b1d3c13aaa16a22d410f292945454885418
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-23 21:33:46 +0100

    add middleware to server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const {chats} = require('./daten/data')
 const dotenv = require("dotenv")
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
+const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 
 dotenv.config()
 connectDB()
@@ -16,6 +17,9 @@ app.get("/", (req,res) =>{
 })
 
 app.use("/api/user", userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
