cbf70ff59a0bc709dec1edc59184e268997e8ef3
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-23 18:46:48 +0100

    Create Schema Models for Database

---
+++ server/model/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    email: {type: String, required: true},
+    pic: {type: String, required: true, default:"https://icon-library.com/images/default-profile-icon/default-profile-icon-6.jpg"},
+},{
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = Message;
