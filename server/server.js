a85f643990654381845f2772df3b4c54547b61e8
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-23 22:08:36 +0100

    configure picture upload

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {chats} = require('./daten/data')
 const dotenv = require("dotenv")
 const connectDB = require('./config/db')
+const multer = require('multer');
 const userRoutes = require('./routes/userRoutes')
 const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 
@@ -11,6 +12,18 @@ const app = express()
 app.use(express.json()) 
 
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads'); // Ensure 'uploads' directory exists
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.use('/api/signup', upload.single('pic'));
 
 app.get("/", (req,res) =>{
     res.send("API is Running")
