b24bcdc306c41bffde7adc75b52334da4c7d544e
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-23 17:24:32 +0100

    Make sure that Mongodb saves messages

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const http = require('http')
 const cors = require('cors')
 const{ Server } =require('socket.io')
 const { connect } = require('./db');
+const Message = require('./model/message')
 
 app.use(cors())
 
@@ -16,23 +17,54 @@ const io = new Server(server, {
     },
 })
 
+let client
+connect()
+    .then((dbClient) => {
+        client = dbClient;
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
 io.on("connection", async (socket) =>{
     console.log(`User Connected: ${socket.id}`)
     
-    await connect();
+    if (!client) {
+        client = await connect();
+    }
 
     socket.on("join_room", (data) =>{
         socket.join(data)
         console.log(`User with ID: ${socket.id} joined room: ${data}`)
     })
 
-    socket.on("send_message" , (data) =>{
-        socket.to(data.room).emit("receive_message", data)
+    socket.on("send_message" , async (data) =>{
+
+        const db = client.db("chatapp");
+        const messagesCollection = client.db("chatapp").collection("messages");
+
+        try{
+            const newMessage = new Message({
+                room: data.room,
+                author: data.username,
+                message: data.message,
+                time: new Date().toISOString(),
+                id: Date.now()
+            })
+
+            await newMessage.save()
+            socket.to(data.room).emit("receive_message", data)
+        } catch (error){
+            console.error("Error saving message to MongoDB: ", error)
+        }
     })
 
     socket.on("disconnect", async ()=>{
         console.log("User Disconnected", socket.id)
-        await client.close();
+        if(client){
+            await client.close()
+            client = null
+        }
     })
 })
 
