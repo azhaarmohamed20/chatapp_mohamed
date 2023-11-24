a3779d67df25894a8fcbaceb26120416b74668b6
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-24 01:15:00 +0100

    Add Remove and Add to GroupChat functionality

--- server/controllers/chatControllers.js
+++ server/controllers/chatControllers.js
@@ -126,6 +126,45 @@ const renameGroup = asyncHandler (async (req, res) => {
         res.json(updatedChat)
     }
 })
+
+const removeFromGroup = asyncHandler (async (req, res) => {
+    const { chatId, userId } = req.body
+
+    const removed = await Chat.findByIdAndUpdate(
+        chatId, 
+        {
+            $pull: { users: userId },
+        }, 
+        { new: true}
+        ).populate("users", "-password").populate("groupAdmin", "-password")
+
+        if(!removed) {
+            res.status(404)
+            throw new Error("Chat Not Found")
+        }else{
+            res.json(removed)
+        } 
+})
+
+
+const addToGroup = asyncHandler (async (req, res) => {
+    const { chatId, userId } = req.body
+
+    const added = await Chat.findByIdAndUpdate(
+        chatId, 
+        {
+            $push: { users: userId },
+        }, 
+        { new: true}
+        ).populate("users", "-password").populate("groupAdmin", "-password")
+
+        if(!added) {
+            res.status(404)
+            throw new Error("Chat Not Found")
+        }else{
+            res.json(added)
+        }
+})
 module.exports = {
-    accessChat,  fetchChats, createGroupChat, renameGroup
+    accessChat,  fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup
 }
