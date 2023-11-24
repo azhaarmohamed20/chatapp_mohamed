0c200dbfbe9c322f08f3c56675200c2143520f55
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-24 01:06:13 +0100

    Add functionality to rename Group

--- server/controllers/chatControllers.js
+++ server/controllers/chatControllers.js
@@ -105,6 +105,27 @@ const createGroupChat = asyncHandler  (async (req, res) => {
     }
 
 })
+
+const renameGroup = asyncHandler (async (req, res) => {
+    const { chatId, chatName } = req.body
+    
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName,
+        },
+        {
+            new: true,
+        }
+    ).populate("users", "-password").populate("groupAdmin", "-password")
+
+    if(!updatedChat) {
+        res.status(404)
+        throw new Error("Chat Not Found")
+    }else{
+        res.json(updatedChat)
+    }
+})
 module.exports = {
-    accessChat,  fetchChats, createGroupChat
+    accessChat,  fetchChats, createGroupChat, renameGroup
 }
