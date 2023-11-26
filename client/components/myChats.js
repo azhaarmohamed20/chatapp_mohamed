f945ccf89eb5ea25f4fdddff89698ee52dbc98ef
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-26 13:13:29 +0100

    implement function to Fetch Chats

--- client/components/myChats.js
+++ client/components/myChats.js
@@ -1,7 +1,46 @@
+"use client"
+import { useToast } from "@chakra-ui/react";
+import { useChatState } from "../context/ChatProvider"
+import { useState } from "react"
+
 export default function MyChats(){
+
+    const[loggedUser, setLoggedUser] = useState();
+    const {
+        setSelectedChat,
+        user,
+        chats,
+        setChats,
+      } = useChatState();
+
+    const toast = useToast();
+
+    const fetchChats = async () => {
+        // console.log(user._id);
+        try {
+          const config = {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          };
+    
+          const { data } = await axios.get("/api/chat", config);
+          setChats(data);
+        } catch (error) {
+          toast({
+            title: "Error Occured!",
+            description: "Failed to Load the chats",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom-left",
+          });
+        }
+      };
+
     return(
         <div>
-            
+            <h2>A Chat</h2>
         </div>
     )
 }
