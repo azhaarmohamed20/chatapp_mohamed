3496bf2347f17cb1370bc5f3892f99d543cff875
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-11-26 16:13:45 +0100

    Create Box for Chats

--- client/components/ChatBox.js
+++ client/components/ChatBox.js
@@ -1,9 +1,21 @@
+"use client"
+import { Box } from "@chakra-ui/react";
+import { useChatState } from "../context/ChatProvider";
 export default function ChatBox() {
+    const { user, selectedChat, socket, onlineUsers } = useChatState();
+
     return (
-        <>
-            <h2>
-                ChatBox
-            </h2>
-        </>
+        <Box
+        d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+        alignItems="center"
+        flexDir="column"
+        p={3}
+        bg="white"
+        w={{ base: "100%", md: "68%" }}
+        borderRadius="lg"
+        borderWidth="1px"
+        >
+            Single Chat
+        </Box>
     );
 }
